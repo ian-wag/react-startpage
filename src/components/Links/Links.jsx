fe9902d45ec78757f1129a9a1b561307f1c91f6f
EN <iwagner76@gmail.com>
2021-09-25 13:45:02 -0400

    changed links urls

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -5,16 +5,16 @@ const Links = () => {
   return (
     <>
       <div className="links">
-        <a href="https://www.reddit.com/">
-          <p className="link-text underline">reddit.</p>
+        <a href="https://www.reddit.com/r/chicago/new/">
+          <p className="link-text underline">r/chicago.</p>
         </a>
-        <a href="https://www.twitch.tv/directory/following">
+        <a href="https://www.reddit.com/r/webdev/">
+          <p className="link-text underline">r/webdev.</p>
+        </a>
+        <a href="https://dashboard.twitch.tv/u/musbynice/stream-manager">
           <p className="link-text underline">twitch.</p>
         </a>
-        <a href="https://twitter.com/home">
-          <p className="link-text underline">twitter.</p>
-        </a>
-        <a href="https://github.com/ian-wag">
+        <a href="https://github.com/ian-wag?tab=repositories">
           <p className="link-text underline">github.</p>
         </a>
       </div>
